3bb4b89cec223cf920b08359ac1683f241d73985
prhmonline <prhmonline@gmail.com>
2026-09-15 02:45:39 +0330

    feat: add readonly bridge metadata guards

--- readonly-bridge-repair-v1.js
+++ readonly-bridge-repair-v1.js
@@ -1,6 +1,8 @@
 #!/usr/local/bin/prhm-node
 'use strict';
 
+const ACTION='readonly_bridge_repair_v1';
+
 function fail(message){throw new Error(message);}
 
 function parseBridgeEnv(text){
@@ -33,4 +35,36 @@ function diffAllowed(before,after){
   try{return rewriteBridgeEnv(before)===after;}catch{return false;}
 }
 
-module.exports={parseBridgeEnv,rewriteBridgeEnv,diffAllowed};
+function validateEnvMetadata(stat){
+  if(!stat||typeof stat!=='object')fail('env_stat_invalid');
+  if(typeof stat.isSymbolicLink==='function'&&stat.isSymbolicLink())fail('env_symlink_forbidden');
+  if(typeof stat.isFile!=='function'||!stat.isFile())fail('env_not_regular_file');
+  if(Number(stat.uid)!==0)fail('env_owner_not_root');
+  if((Number(stat.mode)&0o777)!==0o600)fail('env_mode_not_0600');
+  return true;
+}
+
+function buildPreflightReport(fields){
+  const sha=String(fields&&fields.envSha256||'');
+  if(!/^[0-9a-f]{64}$/.test(sha))fail('env_sha256_invalid');
+  return {
+    ok:true,
+    action:ACTION,
+    schema_version:'prhm.host-action-preflight.v1',
+    preflight_only:true,
+    production_mutation:false,
+    database_mutation:false,
+    recovery_mutation:false,
+    agent_api_mutation:false,
+    mcp_mutation:false,
+    env_sha256:sha,
+    port_key:String(fields.portKey||''),
+    current_port:Number(fields.currentPort),
+    replacement_port:Number(fields.replacementPort),
+    recovery_healthy:fields.recoveryHealthy===true,
+    agent_api_healthy:fields.agentApiHealthy===true,
+    mcp_healthy:fields.mcpHealthy===true
+  };
+}
+
+module.exports={ACTION,parseBridgeEnv,rewriteBridgeEnv,diffAllowed,validateEnvMetadata,buildPreflightReport};
